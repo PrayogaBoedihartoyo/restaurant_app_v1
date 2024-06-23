115349c23f1534b2798c6a587f10fcec893c9c99
Prayoga Boedihartoyo <112841946+PrayogaBoedihartoyo28@users.noreply.github.com>
2024-06-23 15:36:01 +0700

    solve no connection detail page

--- lib/provider/restaurant_provider.dart
+++ lib/provider/restaurant_provider.dart
@@ -7,11 +7,18 @@ enum ResultState { loading, noData, hasData, error }
 class RestaurantProvider extends ChangeNotifier {
   List<Restaurant> _restaurants = [];
   List<Restaurant> _filteredRestaurants = [];
+  RestaurantDetail? _restaurantDetail;
+
   ResultState _state = ResultState.loading;
   String _message = '';
 
-  List<Restaurant> get restaurants => _filteredRestaurants.isEmpty ? _restaurants : _filteredRestaurants;
+  List<Restaurant> get restaurants =>
+      _filteredRestaurants.isEmpty ? _restaurants : _filteredRestaurants;
+
+  RestaurantDetail? get restaurantDetail => _restaurantDetail;
+
   ResultState get state => _state;
+
   String get message => _message;
 
   Future<void> fetchRestaurantsProvider() async {
@@ -42,9 +49,43 @@ class RestaurantProvider extends ChangeNotifier {
     } else {
       _filteredRestaurants = _restaurants
           .where((restaurant) =>
-          restaurant.name.toLowerCase().contains(query.toLowerCase()))
+              restaurant.name.toLowerCase().contains(query.toLowerCase()))
           .toList();
     }
     notifyListeners();
+  }
+
+  Future<void> fetchRestaurantDetail(String id) async {
+    try {
+      _state = ResultState.loading;
+      notifyListeners();
+
+      final result = await detailRestaurantDetail(id);
+      if (result.error) {
+        _state = ResultState.noData;
+        _message = 'Data Restoran Kosong';
+      } else {
+        _restaurantDetail = result;
+        _state = ResultState.hasData;
+      }
+      notifyListeners();
+    } catch (e) {
+      _state = ResultState.error;
+      _message = 'Tidak ada koneksi internet';
+      _restaurantDetail = RestaurantDetail(
+        restaurants: Restaurant(
+          id: '',
+          name: '',
+          description: '',
+          city: '',
+          rating: 0,
+          pictureId: '',
+          categories: [],
+        ),
+        error: true,
+        message: '',
+      );
+      notifyListeners();
+    }
   }
 }
--- lib/ui/restaurant.dart
+++ lib/ui/restaurant.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:restaurant_app/model/restaurant.dart';
 import 'package:restaurant_app/ui/foods.dart';
-
-import '../api/restaurant_api.dart';
+import '../provider/restaurant_provider.dart';
 
 class DetailPage extends StatefulWidget {
   final Restaurant restaurant;
@@ -15,62 +15,55 @@ class DetailPage extends StatefulWidget {
 
 class _DetailPageState extends State<DetailPage> {
   bool showFullDescription = false;
-  RestaurantDetail? futureRestaurant;
 
   @override
   void initState() {
     super.initState();
-    detailFetch();
+    Future.microtask(() =>
+        Provider.of<RestaurantProvider>(context, listen: false)
+            .fetchRestaurantDetail(widget.restaurant.id));
   }
 
-  Future<void> detailFetch() async {
-    final future = detailRestaurantDetail(widget.restaurant.id);
-    future.then((value) {
-      setState(() {
-        futureRestaurant = value;
-      });
-    });
-  }
-
-  Widget header(BuildContext context) {
+  Widget header(BuildContext context, Restaurant restaurant) {
     return SafeArea(
-        child: Hero(
-      tag: widget.restaurant.toString(),
-      child: Container(
-        width: double.infinity,
-        height: 300,
-        decoration: BoxDecoration(
-          borderRadius: const BorderRadius.all(Radius.circular(30)),
-          image: DecorationImage(
-            image: NetworkImage(
-                'https://restaurant-api.dicoding.dev/images/small/${widget.restaurant.pictureId}'),
-            fit: BoxFit.cover,
-          ),
-        ),
+      child: Hero(
+        tag: restaurant.toString(),
         child: Container(
+          width: double.infinity,
+          height: 300,
           decoration: BoxDecoration(
-            borderRadius: const BorderRadius.only(
-              bottomLeft: Radius.circular(20),
-              bottomRight: Radius.circular(20),
+            borderRadius: const BorderRadius.all(Radius.circular(30)),
+            image: DecorationImage(
+              image: NetworkImage(
+                  'https://restaurant-api.dicoding.dev/images/small/${restaurant.pictureId}'),
+              fit: BoxFit.cover,
             ),
-            border: Border.all(
-              color: Colors.white,
-              width: 5,
+          ),
+          child: Container(
+            decoration: BoxDecoration(
+              borderRadius: const BorderRadius.only(
+                bottomLeft: Radius.circular(20),
+                bottomRight: Radius.circular(20),
+              ),
+              border: Border.all(
+                color: Colors.white,
+                width: 5,
+              ),
             ),
           ),
         ),
       ),
-    ));
+    );
   }
 
-  Widget restaurantDetails() {
+  Widget restaurantDetails(Restaurant restaurant, RestaurantDetail? detail) {
     return Padding(
       padding: const EdgeInsets.all(16.0),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           Text(
-            widget.restaurant.name,
+            restaurant.name,
             style: const TextStyle(
               fontSize: 24,
               fontWeight: FontWeight.bold,
@@ -82,7 +75,7 @@ class _DetailPageState extends State<DetailPage> {
               const Icon(Icons.location_on, size: 16),
               const SizedBox(width: 8),
               Text(
-                widget.restaurant.city,
+                restaurant.city,
                 style: const TextStyle(
                   fontSize: 16,
                 ),
@@ -90,7 +83,7 @@ class _DetailPageState extends State<DetailPage> {
             ],
           ),
           const SizedBox(height: 8),
-          _buildRating(double.parse(widget.restaurant.rating.toString())),
+          _buildRating(double.parse(restaurant.rating.toString())),
           const SizedBox(height: 25),
           GestureDetector(
             onTap: () {
@@ -101,8 +94,8 @@ class _DetailPageState extends State<DetailPage> {
             child: RichText(
               text: TextSpan(
                 text: showFullDescription
-                    ? widget.restaurant.description
-                    : _truncateDescription(widget.restaurant.description),
+                    ? restaurant.description
+                    : _truncateDescription(restaurant.description),
                 style: const TextStyle(fontSize: 15, color: Colors.black),
                 children: [
                   TextSpan(
@@ -126,8 +119,8 @@ class _DetailPageState extends State<DetailPage> {
           ),
           const SizedBox(height: 20),
           FoodCarousel(
-              foodList: futureRestaurant?.restaurants.menus?.foods ?? [],
-              restaurant: futureRestaurant?.restaurants),
+              foodList: detail?.restaurants.menus?.foods ?? [],
+              restaurant: detail?.restaurants),
           const SizedBox(height: 20),
           const Text(
             'Drinks',
@@ -138,8 +131,8 @@ class _DetailPageState extends State<DetailPage> {
           ),
           const SizedBox(height: 20),
           FoodCarousel(
-              foodList: futureRestaurant?.restaurants.menus?.drinks ?? [],
-              restaurant: futureRestaurant?.restaurants),
+              foodList: detail?.restaurants.menus?.drinks ?? [],
+              restaurant: detail?.restaurants),
         ],
       ),
     );
@@ -167,26 +160,47 @@ class _DetailPageState extends State<DetailPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.grey[200],
-      body: ListView(
-        children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              IconButton(
-                onPressed: () {
-                  Navigator.pop(context);
-                },
-                icon: const Icon(Icons.arrow_back),
-              ),
-              IconButton(
-                onPressed: () {},
-                icon: const Icon(Icons.favorite_border),
-              ),
-            ],
-          ),
-          header(context),
-          restaurantDetails(),
-        ],
+      body: Consumer<RestaurantProvider>(
+        builder: (context, provider, child) {
+          if (provider.state == ResultState.loading) {
+            return const Center(child: CircularProgressIndicator());
+          } else if (provider.state == ResultState.hasData) {
+            final restaurant = provider.restaurantDetail?.restaurants ??
+                Restaurant(
+                    id: '',
+                    name: '',
+                    description: '',
+                    city: '',
+                    rating: 0,
+                    pictureId: '',
+                    categories: []);
+            return ListView(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    IconButton(
+                      onPressed: () {
+                        Navigator.pop(context);
+                      },
+                      icon: const Icon(Icons.arrow_back),
+                    ),
+                    IconButton(
+                      onPressed: () {},
+                      icon: const Icon(Icons.favorite_border),
+                    ),
+                  ],
+                ),
+                header(context, restaurant),
+                restaurantDetails(restaurant, provider.restaurantDetail),
+              ],
+            );
+          } else if (provider.state == ResultState.error) {
+            return Center(child: Text(provider.message));
+          } else {
+            return const Center(child: Text('Unknown state'));
+          }
+        },
       ),
     );
   }
